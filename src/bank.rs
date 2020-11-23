85523997e59b14e18f8c294555f5603fbcdca136
Zura Benashvili <zura.benashvili@gmail.com>
2020-11-23 13:06:40 +0400

    refactor(bank): move bank (de)serialization logic to `Bank` trait

--- src/bank.rs
+++ src/bank.rs
@@ -1,9 +1,73 @@
+use std::fs::File;
+use std::io;
+use std::convert::TryFrom;
+
+use crate::input_transaction::InputTransaction;
 use crate::transaction::Transaction;
 use crate::account::Account;
+use crate::output_account::OutputAccount;
 
 pub trait Bank: Default {
     type AccountsIter: Iterator<Item = Account>;
+
     /// Apply `Transaction` to the `Account` in `Bank`.
     fn apply_tx<T: Into<Transaction>>(&mut self, tx: T) -> Result<(), ()>;
     fn into_accounts_iter(self) -> Self::AccountsIter;
+
+    /// Reads and deserializes input csv from file and applies
+    /// transactions to the new/empty `Bank`. Returning `Bank`.
+    fn from_input_transactions_csv_file(filename: &str) -> Self {
+        let file = File::open(filename).unwrap();
+        Self::from_input_transactions_csv(file)
+    }
+
+    /// Reads and deserializes input csv from reader and applies
+    /// transactions to the new/empty `Bank`. Returning `Bank`.
+    fn from_input_transactions_csv<R: io::Read>(reader: R) -> Self {
+        let mut rdr = csv::Reader::from_reader(reader);
+
+        Self::from_input_transactions(
+            rdr.deserialize::<InputTransaction>()
+                .filter_map(Result::ok)
+        )
+    }
+
+    /// Applies `InputTransaction`-s to the new/empty `Bank`. Returning `Bank`.
+    fn from_input_transactions<I>(iter: I) -> Self
+    where I: Iterator<Item = InputTransaction>,
+    {
+        let iter = iter
+            .map(Transaction::try_from)
+            .filter_map(Result::ok);
+
+        Self::from_transactions(iter)
+    }
+
+    /// Applies `Transaction`-s to the new/empty `Bank`. Returning `Bank`.
+    fn from_transactions<I>(it: I) -> Self
+    where I: Iterator<Item = Transaction>,
+    {
+        let mut bank = Self::default();
+        it.for_each(|tx: Transaction| {
+            // ignore result
+            let _ = bank.apply_tx(tx);
+        });
+        bank
+    }
+
+
+    /// Extracts accounts data from the bank and serializes
+    /// [OutputAccount](crate::output_account::OutputAccount) to writer.
+    /// No need to create BufWriter since `csv::Writer` uses it's own buffer.
+    fn accounts_to_csv<W>(self, writer: W) -> Result<(), csv::Error>
+    where W: io::Write,
+    {
+        let mut wtr = csv::Writer::from_writer(writer);
+
+        for account in self.into_accounts_iter() {
+            let output: OutputAccount = account.into();
+            wtr.serialize(output)?;
+        }
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use std::convert::TryFrom;
 use std::io;
 use clap::{App, Arg};
 
 mod types;
 mod decimal_serde;
-mod account;
-
-mod transaction;
-use transaction::Transaction;
-
-mod output_account;
-use output_account::OutputAccount;
-
 mod input_transaction;
-use input_transaction::InputTransaction;
+mod transaction;
+mod account;
+mod output_account;
 
 mod bank;
 use bank::Bank;
@@ -23,37 +16,6 @@ use basic_bank::BasicBank;
 
 mod concurrent_bank;
 use concurrent_bank::ConcurrentBank;
-
-/// Parses input csv and applies transactions to the new/empty `Bank`.
-fn bank_from_transactions_csv<B: Bank>(filename: &str) -> B {
-    let mut bank = B::default();
-    let mut rdr = csv::Reader::from_path(filename).unwrap();
-
-    rdr.deserialize::<InputTransaction>()
-        .filter_map(Result::ok)
-        .map(Transaction::try_from)
-        .filter_map(Result::ok)
-        .for_each(|tx: Transaction| {
-            // ignore result
-            let _ = bank.apply_tx(tx);
-        });
-
-    bank
-}
-
-/// Extracts accounts data from the bunk and serializes to writer.
-/// No need to create BufWriter since `csv::Writer` uses it's own buffer.
-fn bank_accounts_to_csv<B, W>(bank: B, writer: W)
-where B: Bank,
-      W: io::Write,
-{
-    let mut wtr = csv::Writer::from_writer(writer);
-
-    for account in bank.into_accounts_iter() {
-        let output: OutputAccount = account.into();
-        wtr.serialize(output).unwrap();
-    }
-}
 
 fn main() {
     // parse cli args
@@ -74,10 +36,10 @@ fn main() {
     let is_concurrent = matches.is_present("concurrent");
 
     if !is_concurrent {
-        let bank = bank_from_transactions_csv::<BasicBank>(filename);
-        bank_accounts_to_csv(bank, io::stdout().lock());
+        let bank = BasicBank::from_input_transactions_csv_file(filename);
+        bank.accounts_to_csv(io::stdout().lock()).unwrap();
     } else {
-        let bank = bank_from_transactions_csv::<ConcurrentBank>(filename);
-        bank_accounts_to_csv(bank, io::stdout().lock());
+        let bank = ConcurrentBank::from_input_transactions_csv_file(filename);
+        bank.accounts_to_csv(io::stdout().lock()).unwrap();
     }
 }
