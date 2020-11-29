34399bbe85677de7e12f93ed1cf161a13997edc5
Zura Benashvili <zura.benashvili@gmail.com>
2020-11-29 08:55:58 +0400

    refactor(bank): extract `BankThread` logic from `ConcurrentBank`

--- src/concurrent_bank.rs
+++ src/concurrent_bank.rs
@@ -8,10 +8,52 @@ use crate::bank::Bank;
 use crate::basic_bank::BasicBank;
 
 struct BankThread {
-    pub thread: thread::JoinHandle<BasicBank>,
-    pub tx: mpsc::Sender<Transaction>,
+    thread: Option<thread::JoinHandle<BasicBank>>,
+    sender: Option<mpsc::Sender<Transaction>>,
 }
 
+impl BankThread {
+    pub fn new() -> Self {
+        let (sender, rx) = mpsc::channel();
+        let thread = thread::spawn(move || {
+            let mut bank = BasicBank::new();
+            while let Ok(tx) = rx.recv() {
+                // ignore result
+                let _ = bank.apply_tx(tx);
+            }
+            bank
+        });
+
+        BankThread {
+            sender: Some(sender),
+            thread: Some(thread),
+        }
+    }
+
+    pub fn apply_tx(&mut self, tx: Transaction) {
+        if let Some(sender) = &self.sender {
+            let _ = sender.send(tx);
+        }
+    }
+
+    pub fn join(&mut self) -> Option<BasicBank> {
+        match (self.thread.take(), self.sender.take()) {
+            (Some(thread), Some(sender)) => {
+                // drop `Sender` to let thread no that it's
+                // work is finished and it can return.
+                drop(sender);
+                thread.join().ok()
+            },
+            _ => None,
+        }
+    }
+}
+
+impl Drop for BankThread {
+    fn drop(&mut self) {
+        let _ = self.join();
+    }
+}
 
 /// Stores and manages accounts in the bank **Concurrently**.
 ///
@@ -38,21 +80,14 @@ impl Bank for ConcurrentBank {
         let tx: Transaction = tx.into();
         let bank_thread = self.get_thread_for_client_mut(tx.get_client_id());
 
-        let _ = bank_thread.tx.send(tx);
+        bank_thread.apply_tx(tx);
         Ok(())
     }
 
     /// Consumes `ConcurrentBank` and **Blocks** untill all threads finish.
     /// Outputs `Account` iterator.
     fn into_accounts_iter(self) -> Self::AccountsIter {
-        let iter = self.threads.into_iter()
-            .map(|bank_thread| {
-                let BankThread { thread, tx } = bank_thread;
-                // drop `Sender` to let thread no that it's
-                // work is finished and it can return.
-                drop(tx);
-                thread.join().unwrap()
-            })
+        let iter = self.into_inner_banks()
             .map(|bank| bank.into_accounts_iter())
             .flatten();
         Box::new(iter)
@@ -70,19 +105,8 @@ impl ConcurrentBank {
     pub fn new_with_thread_count(count: usize) -> Self {
         Self {
             count,
-            threads: (0..count).map(|_| {
-                let (tx, rx) = mpsc::channel();
-                let thread = thread::spawn(move || {
-                    let mut bank = BasicBank::new();
-                    while let Ok(transaction) = rx.recv() {
-                        // ignore result
-                        let _ = bank.apply_tx(transaction);
-                    }
-                    bank
-                });
-
-                BankThread { tx, thread }
-            }).collect(),
+            threads: (0..count)
+                .map(|_| BankThread::new()).collect(),
         }
     }
 
@@ -94,5 +118,10 @@ impl ConcurrentBank {
         client_id: ClientID
     ) -> &mut BankThread {
         &mut self.threads[(client_id as usize) % self.count]
+    }
+
+    fn into_inner_banks(self) -> impl Iterator<Item = BasicBank> {
+        self.threads.into_iter()
+            .map(|mut bank_thread| bank_thread.join().unwrap())
     }
 }
